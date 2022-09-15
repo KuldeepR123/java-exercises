557408d0eebd459066cc5902b9dcd1dfd09e5930
Kuldeep <kuldeepr@prominentpixel.com>
2022-09-15 11:52:52 +0530

    OddEven Threading

--- src/main/java/com/example/threading/ThreadExercise1.java
+++ src/main/java/com/example/threading/ThreadExercise1.java
@@ -1,26 +1,5 @@
 package com.example.threading;
-class OddThread extends Thread {
-    public OddThread(String name) {
-        super(name);
-    }
-    @Override
-    public void run() {
-        for (int i = 0; i <= 1000; i++) {
-            if (i % 2 != 0) {
-                System.out.println(Thread.currentThread().getName() + " --> " +  i);
-            }
-        }
-    }
-}
-class EvenThread implements Runnable {
-    public void run() {
-        for (int i = 0; i <= 1000; i++) {
-            if (i % 2 == 0 && i!=0) {
-                System.out.println(Thread.currentThread().getName() + " --> " +  i);
-            }
-        }
-    }
-}
+
 public class ThreadExercise1{
     public static void main(String[] args) {
         OddThread oddThread = new OddThread("Odd Thread");
